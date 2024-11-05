89dd57ed4792043380d2fb6912cf5e2e63d2d906
user_undeclared <93453842+user-undeclared@users.noreply.github.com>
2024-11-05 05:12:55 -0700

    update example-2 to show array scope in the code

--- example-2.c
+++ example-2.c
@@ -4,6 +4,29 @@
 #include <ctype.h>
 #include <stdio.h>
 
+char* repeat_string(const char* string, unsigned repeat_count);
+
+int main(void) {
+    const char* const message = "happy happy joy joy ";
+    const unsigned message_repetition_count = 4;
+    unsigned character_index;
+    char* character;
+
+    char* repeated_message = repeat_string(message, message_repetition_count);
+    if(repeated_message == NULL) {
+        return 1;
+    }
+    for(character_index = 0; character_index < strlen(repeated_message); character_index++) {
+        character = &repeated_message[character_index];
+        *character = toupper(*character);
+    }
+
+    puts(repeated_message);
+
+    free(repeated_message);
+    return 0;
+}
+
 #define DYNAMIC_ARRAY_IMPLEMENTATION
 #include "dynamic_array.h"
 
@@ -34,25 +57,4 @@ char* repeat_string(const char* string, unsigned repeat_count) {
     repeated_string = dynamic_array_push(repeated_string, &nul);
 
     return dynamic_array_to_heap_allocated_array(repeated_string);
-}
-
-int main(void) {
-    const char* const message = "happy happy joy joy ";
-    const unsigned message_repetition_count = 4;
-    unsigned character_index;
-    char* character;
-
-    char* repeated_message = repeat_string(message, message_repetition_count);
-    if(repeated_message == NULL) {
-        return 1;
-    }
-    for(character_index = 0; character_index < strlen(repeated_message); character_index++) {
-        character = &repeated_message[character_index];
-        *character = toupper(*character);
-    }
-
-    puts(repeated_message);
-
-    free(repeated_message);
-    return 0;
 }
